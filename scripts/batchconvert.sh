bcf93cd27757177a6164bbe50e259f5fd242b6ce
MarcelCosta79 <52638078+MarcelCosta79@users.noreply.github.com>
2020-03-24 18:27:24 -0300

    Delete batchconvert.sh

--- scripts/batchconvert.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-fpath="$sonarr_episodefile_path"
-ss=$(dirname "$fpath")
-cd "$ss"
-find -type f ! -name "*edited.mkv" -size +100M -printf '\033[32m%p\033[0m\n'
-find -type f ! -name "*edited.mkv" -size +100M -exec /scripts/one.sh {} \; 
