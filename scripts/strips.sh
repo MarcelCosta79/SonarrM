b1073afa90b810692d9c630878c3fbd2cd3e3eaa
mco_007 <mco_007@users.noreply.github.com>
2020-03-25 22:35:35 -0300

    Update strips.sh
    more stuff

--- scripts/strips.sh
+++ scripts/strips.sh
@@ -7,9 +7,9 @@
 # I've just made some tweaks.                                      #
 ####################################################################
 
-######  PushOver API  ############ So altere esses campos #############
-APP_TOKEN="aur35g2u3o7k6wy6rg5rcuoh4nokxr"
-USER_TOKEN="ukov8ca9heddn3f429qr212w2a51za"
+###############  PushOver API  ############ So altere esses campos #
+APP_TOKEN="YOUR_TOKEN_HERE"
+USER_TOKEN="YOUR_USER_ID_HERE"
 ####################################################################
 
 
@@ -31,17 +31,16 @@ cd "$ss"
   
    diffaudio=$(expr $totalaudio - $audiocount)
    diffsubs=$(expr $totalsubs - $subscount)
-     if [ -z "$subs" ] # Se $Subs for vazio executa - Executa caso não haja nenhum sub válido.
+     if [ -z "$subs" ] # Loop if there is no valid subtitle.
      then
-       echo "3: Nothing to remove, will look for ASS & PGS Files"
        audio=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: audio .* language:\(por\|eng\|jpn\|und\).*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
        echo "4: found $audio to keep"
        subs=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: subtitles [(SubStationAlpha)|(ASS)|(HDMV/PGS)|(VobSub)].*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
        echo "5: found $subs to remove"
 
-       if [ -z "$subs" ] # Executa se não tiver nenhum sub não válido
+       if [ -z "$subs" ] # Loop if there is no invalid subtitle.
        then
-   			if [ $diffaudio -gt 0 ] # Executa se tiver mais áudio que áudio válidos
+   			if [ $diffaudio -gt 0 ] # Loop if there are more audio than valid audios.
 			then
 				echo diffaudio= $diffaudio
 				echo "6: Only needed audio found."
@@ -51,15 +50,15 @@ cd "$ss"
 				mv "${file%.mkv}".edited.mkv "$file"
 				echo "7: Unwanted audio found and removed!"
 				# mv "$1" /media/Trash/;
-				if [ $APP_TOKEN != "YOUR_TOKEN_HERE" ]
+				if [ $APP_TOKEN != "YOUR_TOKEN_HERE" ] #Don't alter
 				then 
-					wget https://api.pushover.net/1/messages.json --post-data="token=$APP_TOKEN&user=$USER_TOKEN=$file - Audio removido!&title=SonarrM" -qO- > /dev/null 2>&1 &
+					wget https://api.pushover.net/1/messages.json --post-data="token=$APP_TOKEN&user=$USER_TOKEN=$file - Audio Removed!&title=SonarrM" -qO- > /dev/null 2>&1 &
 				fi
 			else
 				echo "6: Nothing found to remove. Will exit script now."
-				if [ $APP_TOKEN != "YOUR_TOKEN_HERE" ]
+				if [ $APP_TOKEN != "YOUR_TOKEN_HERE" ] #Don't alter
 				then 
-					wget https://api.pushover.net/1/messages.json --post-data="token=$APP_TOKEN&user=$USER_TOKEN&message=$file - Nada para remover&title=SonarrM" -qO- > /dev/null 2>&1 &
+					wget https://api.pushover.net/1/messages.json --post-data="token=$APP_TOKEN&user=$USER_TOKEN&message=$file - Nothing found to remove.&title=SonarrM" -qO- > /dev/null 2>&1 &
 				fi
 			fi
        else
@@ -69,18 +68,18 @@ cd "$ss"
          mv "${file%.mkv}".edited.mkv "$file"
          echo "7: PGS/ASS/VobSub Subtitles found and removed!"
          # mv "$1" /media/Trash/;
-		 if [ $APP_TOKEN != "YOUR_TOKEN_HERE" ]
+		 if [ $APP_TOKEN != "YOUR_TOKEN_HERE" ] #Don't alter
 			then 
-				wget https://api.pushover.net/1/messages.json --post-data="token=$APP_TOKEN&user=$USER_TOKEN&message=$file - Legendas, PGS/ASS/VobSuB, removidas&title=SonarrM" -qO- > /dev/null 2>&1 &
+				wget https://api.pushover.net/1/messages.json --post-data="token=$APP_TOKEN&user=$USER_TOKEN&message=$file - Unwanted subtitles removed&title=SonarrM" -qO- > /dev/null 2>&1 &
 			fi
        fi
 	
 	 elif [ $diffsubs -eq 0 -a $diffaudio -eq 0 ]
 	 then
 	   echo "3: Only needed audio and subtitles found" 
-	   if [ $APP_TOKEN != "YOUR_TOKEN_HERE" ]
+	   if [ $APP_TOKEN != "YOUR_TOKEN_HERE" ] #Don't alter
 		then 
-				wget https://api.pushover.net/1/messages.json --post-data="token=$APP_TOKEN&user=$USER_TOKEN&message=$file - Nada para alterar.&title=SonarrM" -qO- > /dev/null 2>&1 &
+				wget https://api.pushover.net/1/messages.json --post-data="token=$APP_TOKEN&user=$USER_TOKEN&message=$file - Nothing found to remove.&title=SonarrM" -qO- > /dev/null 2>&1 &
 		fi
 	  
      else
@@ -91,9 +90,16 @@ cd "$ss"
        mkvmerge $subs $audio -o "${file%.mkv}".edited.mkv "$file";
        mv "${file%.mkv}".edited.mkv "$file"
        # mv "$1" /media/Trash/;
-	   if [ $APP_TOKEN != "YOUR_TOKEN_HERE" ]
-		then 
-					wget https://api.pushover.net/1/messages.json --post-data="token=$APP_TOKEN&user=$USER_TOKEN&message=$file - Legendas localizadas.&title=SonarrM" -qO- > /dev/null 2>&1 &
+	   if [ $APP_TOKEN != "YOUR_TOKEN_HERE" ] #Don't alter
+			if [ $diffsubs -gt 0 -a $diffaudio -gt 0 ]
+			then 
+					wget https://api.pushover.net/1/messages.json --post-data="token=$APP_TOKEN&user=$USER_TOKEN&message=$file - Subtitles removed.&title=SonarrM" -qO- > /dev/null 2>&1 &
+			elif [ $diffaudio -gt 0 ]
+			then
+					wget https://api.pushover.net/1/messages.json --post-data="token=$APP_TOKEN&user=$USER_TOKEN&message=$file - Subtitles removed.&title=SonarrM" -qO- > /dev/null 2>&1 &
+			elif [ $diffsubs -gt 0 -a $diffaudio -gt 0 ]
+			then	
+					wget https://api.pushover.net/1/messages.json --post-data="token=$APP_TOKEN&user=$USER_TOKEN&message=$file - Subtitles removed.&title=SonarrM" -qO- > /dev/null 2>&1 &
 		fi
      fi
 exit
